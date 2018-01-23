56d2768b4be1bb2856df1d240a2781ccf91b39d2
Shubham <shubham8069@gmail.com>
2018-01-23 18:15:19 +0530

    Modified code

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,8 +5,8 @@ from .models import Book, Author, Genre, BookInstance
 
 def index(request):
 
-	num_books = Book.objects.all.count()
-	num_instances = BookInstance.objects.all.count()
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
 
 	num_instances_avilable = BookInstance.objects.filter(status__exact='a').count()
 	num_authors = Author.objects.count()
